f821e4579d0ebeafb6424643e13497d81c419336
mahmut-yigit <60403058+mahmut-yigit@users.noreply.github.com>
2020-03-17 02:36:36 +0200

    changes osman

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -8,6 +8,9 @@ public class GitDay4 {
 	public static void main(String[] args) {
 		
 		System.out.println("local first commit master branch");
+		System.out.println("Someone else contrubited code ");
+		
+		
 		
 
 	}
